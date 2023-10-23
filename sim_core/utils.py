8596141f395d09c8674bc1805eb4d898cdcfb42d
Nelson-Jnrnd <nelsonjeanrenaud@gmail.com>
2023-10-23 10:25:02 +0200

    Extracted fuzzy logic to utils

--- sim_core/utils.py
+++ sim_core/utils.py
@@ -64,3 +64,54 @@ def plot_torque_curve(engine):
     ax.set_xlabel('Speed (RPM)')
     ax.set_ylabel('Torque (Nm)')
     return fig, ax
+
+# ---- Speed Fuzzy logic ----
+
+def trapezoidal_membership(speed, start, rise_start, fall_start, end):
+    """
+    Calculates the membership value of a speed using a trapezoidal membership function.
+    
+    Parameters:
+    - speed: The speed for which to calculate the membership value.
+    - start: The start of the trapezoid where the membership is 0.
+    - rise_start: The point at which the membership starts rising to 1.
+    - fall_start: The point at which the membership starts falling from 1.
+    - end: The end of the trapezoid where the membership is 0.
+    
+    Returns:
+    - The membership value of the speed.
+    """
+    if speed <= start or speed >= end:
+        return 0
+    elif start < speed <= rise_start:
+        return (speed - start) / (rise_start - start)
+    elif rise_start < speed < fall_start:
+        return 1
+    elif fall_start <= speed < end:
+        return (end - speed) / (end - fall_start)
+    else:
+        return 0
+
+# Speed values (in kph) delimiting the speed bins
+START = 0
+START_MEDIUM = 75
+END_SLOW = 125
+START_HIGH = 150
+END_MEDIUM = 200
+END = 500
+
+# Define membership functions for different speed categories
+def low_speed(speed):
+    return trapezoidal_membership(speed, START, START, START_MEDIUM, END_SLOW)
+
+def medium_speed(speed):
+    return trapezoidal_membership(speed, START_MEDIUM, END_SLOW, START_HIGH, END_MEDIUM)
+
+def high_speed(speed):
+    return trapezoidal_membership(speed, START_HIGH, END_MEDIUM, END, END)
+
+def speed_membership(speed):
+    """
+    Returns the membership values for a given speed in the three sets: Low, Medium, and High.
+    """
+    return low_speed(speed), medium_speed(speed), high_speed(speed)
